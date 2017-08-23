e121ee14eb0f556729e8cdd9c8efac25c5a0217d
Dre mf Randaci <drerandaci@gmail.com>
2017-08-22 20:15:57 -0500

    added cat cards

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("Hey there")
+
+let catsArray = [
+{
+	name: 'Glower', 
+	color: 'Green', 
+	url:'https://catsaspets.files.wordpress.com/2010/04/greenpetsecoliving.jpg',
+	sport: 'Web Developer'
+},
+{
+	name: 'Slugger', 
+	color: 'Red', 
+	url:'http://amolife.com/image/images/stories/Animals/Cats/red_cat_%20(19).jpg',
+	sport: 'Actor'
+},
+{
+	name: 'FrooFroo', 
+	color: 'Pink', 
+	url:'https://i.pinimg.com/736x/29/1b/3d/291b3de6fc97709bb4799b3ecc8f4989--pink-cat-funny-stuff.jpg',
+	sport: 'Kim Kardashian Lookalike'
+}];
+
+let container = document.getElementById('container');
+
+function domString(catsArray) {
+	let string = '';
+	for (var i = 0; i < catsArray.length; i++) {
+	string += `<div id='catCards'>`;
+	string += 	`<h1>${catsArray[i].name}</h1>`;
+	string += 	`<img src='${catsArray[i].url}'>`;
+	string += 	`<h4>${catsArray[i].sport}</h4>`;
+	string += 	`<p>${catsArray[i].color}</p>`;
+	string += `</div>`
+	writeToDom(string);
+	};
+}
+
+function writeToDom(string) {
+	container.innerHTML = string;
+}
+
+domString(catsArray);
