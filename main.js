d18c0905102d806be1c3b397aaa37dc0f4d728d3
Dre mf Randaci <drerandaci@gmail.com>
2017-08-22 21:12:00 -0500

    more json functionality

--- main.js
+++ main.js
@@ -12,10 +12,37 @@ function domString(catsArray) {
 	string += `</div>`
 	writeToDom(string);
 	};
-}
+};
 
 function writeToDom(string) {
 	container.innerHTML = string;
-}
+};
 
-domString(catsArray);
+function executeThisCodeAfterFileLoads1() {
+	// console.log('this', this.responseText);
+	let data = JSON.parse(this.responseText)
+	domString(data.catsArray);
+};
+
+//the the dogs JSON hasnt been created yet to run this function
+function executeThisCodeAfterFileLoads2() {
+	// console.log('this', this.responseText);
+	let data = JSON.parse(this.responseText)
+	domString(data.dogsArray);
+};
+
+function executeThisCodeIfFileErrors() {
+	console.log('Error');
+};
+
+let myRequest1 = new XMLHttpRequest();
+myRequest1.addEventListener('load', executeThisCodeAfterFileLoads1);
+myRequest1.addEventListener('error', executeThisCodeIfFileErrors);
+myRequest1.open('GET', 'cats.json');
+myRequest1.send();
+
+let myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener('load', executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener('error', executeThisCodeIfFileErrors);
+myRequest2.open('GET', 'dogs.json');
+myRequest2.send();
